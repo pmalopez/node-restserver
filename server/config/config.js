646359390016b4b28bc100d0db8065efc71ed364
Marvin <marvin.a.lopez@gmail.com>
2020-05-24 20:19:42 -0600

    'RestServer'

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,24 @@
-//
-// port
-//
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    //    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://pmalopez:X6jDug5Fdq9wpBKb@cluster0-ckaw3.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;

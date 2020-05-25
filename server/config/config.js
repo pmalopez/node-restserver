fed0712795aec51f520794ce301ac98b5d5926c1
Marvin <marvin.a.lopez@gmail.com>
2020-05-24 20:46:11 -0600

    Variable de entorno base de datos mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //    urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb+srv://pmalopez:X6jDug5Fdq9wpBKb@cluster0-ckaw3.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;

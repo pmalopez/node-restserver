ac7ec6d44cb9b72ba15241b35cb43bd6385ec962
Marvin <marvin.a.lopez@gmail.com>
2020-05-25 17:20:14 -0600

    Login normal seccion 10

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,13 +1,20 @@
 const express = require('express');
+
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const Usuario = require('../models/usuario');
+
 const app = express();
+
+
 
 app.post('/login', (req, res) => {
 
     let body = req.body;
 
     Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+
         if (err) {
             return res.status(500).json({
                 ok: false,
@@ -19,28 +26,40 @@ app.post('/login', (req, res) => {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: '(Usuario) o contrase;a incorrectos'
+                    message: '(Usuario) o contraseña incorrectos'
                 }
             });
         }
 
-        if (bcrypt.compareSync(body.password, usuarioDB.password)) {
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'Usuario o (contrase;a) incorrectos'
+                    message: 'Usuario o (contraseña) incorrectos'
                 }
             });
         }
+
+        let token = jwt.sign({
+            usuario: usuarioDB
+        }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
 
         res.json({
             ok: true,
             usuario: usuarioDB,
-            token: '123'
+            token
         });
+
+
     });
 
 });
+
+
+
+
+
 
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -12,8 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-// configuracion global de rutas
+
+// Configuración global de rutas
 app.use(require('./routes/index'));
+
+
 
 mongoose.connect(process.env.URLDB, (err, res) => {
 
